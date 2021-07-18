f7c450c4b6204a005dcefa40da14dfc6795550be
dtinth on MBP M1 <dtinth@spacet.me>
2021-07-19 04:58:31 +0700

    Fonts and borders

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,6 +12,10 @@ export default class MyDocument extends Document {
               rel='shortcut icon'
               href='https://bemuse.ninja/res/favicon.png'
             />
+            <link
+              href='https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,400;0,700;1,400&display=swap'
+              rel='stylesheet'
+            />
           </Head>
 
           <body>
